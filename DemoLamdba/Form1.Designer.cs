2f10722c88d9956e01aed31bbb9f132104174ecc
DBI/Wilber.Padilla <wpadilla@info-arch.com>
2018-12-13 18:07:41 -0400

    DemoLambda Project was added

---
+++ DemoLamdba/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Lambdas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btntraditionaldelegate = new System.Windows.Forms.Button();
+            this.btnanonymousmethod = new System.Windows.Forms.Button();
+            this.btnsimplelamdaexpression = new System.Windows.Forms.Button();
+            this.btnlamdawithoutinference = new System.Windows.Forms.Button();
+            this.btnlamdawithmultipleparameters = new System.Windows.Forms.Button();
+            this.btnlambdawithcodeblock = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btntraditionaldelegate
+            // 
+            this.btntraditionaldelegate.Location = new System.Drawing.Point(47, 24);
+            this.btntraditionaldelegate.Name = "btntraditionaldelegate";
+            this.btntraditionaldelegate.Size = new System.Drawing.Size(296, 23);
+            this.btntraditionaldelegate.TabIndex = 0;
+            this.btntraditionaldelegate.Text = "Traditional Delegate";
+            this.btntraditionaldelegate.UseVisualStyleBackColor = true;
+            this.btntraditionaldelegate.Click += new System.EventHandler(this.btntraditionaldelegate_Click);
+            // 
+            // btnanonymousmethod
+            // 
+            this.btnanonymousmethod.Location = new System.Drawing.Point(47, 53);
+            this.btnanonymousmethod.Name = "btnanonymousmethod";
+            this.btnanonymousmethod.Size = new System.Drawing.Size(296, 23);
+            this.btnanonymousmethod.TabIndex = 1;
+            this.btnanonymousmethod.Text = "Anonymous Method";
+            this.btnanonymousmethod.UseVisualStyleBackColor = true;
+            this.btnanonymousmethod.Click += new System.EventHandler(this.btnanonymousmethod_Click);
+            // 
+            // btnsimplelamdaexpression
+            // 
+            this.btnsimplelamdaexpression.Location = new System.Drawing.Point(47, 85);
+            this.btnsimplelamdaexpression.Name = "btnsimplelamdaexpression";
+            this.btnsimplelamdaexpression.Size = new System.Drawing.Size(296, 23);
+            this.btnsimplelamdaexpression.TabIndex = 2;
+            this.btnsimplelamdaexpression.Text = "Simple Lambda Expression";
+            this.btnsimplelamdaexpression.UseVisualStyleBackColor = true;
+            this.btnsimplelamdaexpression.Click += new System.EventHandler(this.btnsimplelamdaexpression_Click);
+            // 
+            // btnlamdawithoutinference
+            // 
+            this.btnlamdawithoutinference.Location = new System.Drawing.Point(47, 114);
+            this.btnlamdawithoutinference.Name = "btnlamdawithoutinference";
+            this.btnlamdawithoutinference.Size = new System.Drawing.Size(296, 23);
+            this.btnlamdawithoutinference.TabIndex = 3;
+            this.btnlamdawithoutinference.Text = "Lambda Without Inference";
+            this.btnlamdawithoutinference.UseVisualStyleBackColor = true;
+            this.btnlamdawithoutinference.Click += new System.EventHandler(this.btnlamdawithoutinference_Click);
+            // 
+            // btnlamdawithmultipleparameters
+            // 
+            this.btnlamdawithmultipleparameters.Location = new System.Drawing.Point(47, 143);
+            this.btnlamdawithmultipleparameters.Name = "btnlamdawithmultipleparameters";
+            this.btnlamdawithmultipleparameters.Size = new System.Drawing.Size(296, 23);
+            this.btnlamdawithmultipleparameters.TabIndex = 4;
+            this.btnlamdawithmultipleparameters.Text = "Lambda With Multiple Parameters";
+            this.btnlamdawithmultipleparameters.UseVisualStyleBackColor = true;
+            this.btnlamdawithmultipleparameters.Click += new System.EventHandler(this.btnlamdawithmultipleparameters_Click);
+            // 
+            // btnlambdawithcodeblock
+            // 
+            this.btnlambdawithcodeblock.Location = new System.Drawing.Point(47, 172);
+            this.btnlambdawithcodeblock.Name = "btnlambdawithcodeblock";
+            this.btnlambdawithcodeblock.Size = new System.Drawing.Size(296, 23);
+            this.btnlambdawithcodeblock.TabIndex = 5;
+            this.btnlambdawithcodeblock.Text = "Lambda With Code Block";
+            this.btnlambdawithcodeblock.UseVisualStyleBackColor = true;
+            this.btnlambdawithcodeblock.Click += new System.EventHandler(this.btnlambdawithcodeblock_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 223);
+            this.Controls.Add(this.btnlambdawithcodeblock);
+            this.Controls.Add(this.btnlamdawithmultipleparameters);
+            this.Controls.Add(this.btnlamdawithoutinference);
+            this.Controls.Add(this.btnsimplelamdaexpression);
+            this.Controls.Add(this.btnanonymousmethod);
+            this.Controls.Add(this.btntraditionaldelegate);
+            this.Name = "Form1";
+            this.Text = "Lambdas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntraditionaldelegate;
+        private System.Windows.Forms.Button btnanonymousmethod;
+        private System.Windows.Forms.Button btnsimplelamdaexpression;
+        private System.Windows.Forms.Button btnlamdawithoutinference;
+        private System.Windows.Forms.Button btnlamdawithmultipleparameters;
+        private System.Windows.Forms.Button btnlambdawithcodeblock;
+    }
+}
+
---
+++ DemoLamdba/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lambdas
+{
+    public partial class Form1 : Form
+    {
+        delegate int Transformer(int x);
+        delegate int Add(int x, int y);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //This uses a traditional delegate to call a method to square a number
+        private void btntraditionaldelegate_Click(object sender, EventArgs e)
+        {
+            Transformer method = Square; //Defines a delegate and points it to a method named Square
+            int result = method(5);  //Calles the method Square by using the delegate name and stores the result in a variable named "result"
+            MessageBox.Show(result.ToString());  //Displays the result in a message box
+        }
+
+        //This produces the same result as the above method, but uses an anonymous method
+        private void btnanonymousmethod_Click(object sender, EventArgs e)
+        {
+            Transformer sqr = delegate (int x) //This declares a delegate as the above method except that it uses an anonymous method instead of
+              {                                //pointing to a regular method, which makes the logic easier to follow
+                  return x * x;
+              };
+            int result = sqr(5);
+            MessageBox.Show(result.ToString());  //This calls the anonymous method via the delegate name and displays the result in a message box
+        }
+
+        //This produces the same output as the above methods except lambda expressions instead of traditional dellegates or anonyous methods
+        private void btnsimplelamdaexpression_Click(object sender, EventArgs e)
+        {
+            Transformer sqr = x => x * x; //Declares a delegate, but uses a lambda expression instead of an anonymous method.  This makes the code more concise
+            MessageBox.Show(sqr(5).ToString());  //Executes the lambda expression via the delegate name and displays the output in a messagebox
+        }
+
+        //Exactly the ame as the previous method.  The only difference is the parameter of the lambda explictly declares the data type.
+        private void btnlamdawithoutinference_Click(object sender, EventArgs e)
+        {
+            Transformer sqr = (int x) => x * x; //When explicitly declaring the data type. parenthesis are required
+            MessageBox.Show(sqr(5).ToString());  //Executes the lambda expression via the delegate name and displays  the output in a message box
+        }
+
+        //This creates a lambda that adds 2 numbers together.  It demonstrates a lambda with more than 1 parameter
+        private void btnlamdawithmultipleparameters_Click(object sender, EventArgs e)
+        {
+            Add sum = (x, y) => x + y;  //When creating a lambda with more than 1 parameter, parenthesis are required regardless of whether or not the data types are inferred
+            MessageBox.Show(sum(2, 2).ToString());  //Executes the lambda via the delegate name and displays the output in a message box
+        }
+
+        //Same as previous method but demonstates a lambda using a code block when more than 1 line of code is needed.
+        private void btnlambdawithcodeblock_Click(object sender, EventArgs e)
+        {
+            Add sum = (x, y) =>  //Code block is required when more than one line of code is needed.  Notice the ; after the closing curly brace
+              {
+                  return x + y;
+              };
+            MessageBox.Show(sum(2, 2).ToString());  //Executes the lambda via the delegate name and displays the output in a message box
+        }
+
+        //This is the method that's being pointed to using the traditional delegate technique
+        private int Square(int x)
+        {
+            return x * x;
+        }
+        
+        //C# 7 method with only one line can be written like this
+        //private int Square(int x) => x * x;
+    }
+}
